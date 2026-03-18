ac4e99fa700781b92cdd180b435448f28c8669fc
szymoonio <zyszeksimon2005@gmail.com>
2026-03-18 12:38:14 +0100

    Add player and board functionality

--- board.rb
+++ board.rb
@@ -1,12 +1,25 @@
 class Board
-  attr_accessor :rows
+  attr_accessor :rows, :game_over
   def initialize
     self.rows = Array.new(3) {Array.new(3, ' ')}
+    self.game_over = false
   end
   def show_state
     rows.each do |row|
       puts " #{row[0]} | #{row[1]} | #{row[2] }"
       puts "-----------"
+    end
+  end
+  def change_state(player, r, c)
+    self.rows[r][c] = player.sign
+    if rows[r][0] == rows[r][1] && rows[r][1] == rows[r][2]
+      self.game_over = true
+    elsif rows[0][c] == rows[1][c] && rows[1][c] == rows[2][c]
+      self.game_over = true
+    elsif rows[0][0] == rows[1][1] && rows[1][1] == rows[2][2]
+      self.game_over = true
+    elsif rows[0][2] == rows[1][1] && rows[1][1] == rows[2][0]
+      self.game_over = true
     end
   end
 end
--- player.rb
+++ player.rb
@@ -1,7 +1,20 @@
 class Player
-  attr_accessor :name, :result
-  def initialize(name)
+  @@players = 0
+
+  def self.get_pc
+    @@players
+  end
+
+  attr_accessor :name, :sign, :result
+
+  def initialize(name, sign)
+    @@players += 1
     self.name = name
+    if @@players == 1
+      self.sign = '+'
+    else 
+      self.sign = 'o'
+    end
     self.result = 'tbd'
   end
 end
